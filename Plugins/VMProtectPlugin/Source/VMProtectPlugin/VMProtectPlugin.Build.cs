d1bccdadba81108acac138ea6f4a1211a98e9a6c
badlabor <badlabor@work.com>
2016-12-13 12:21:35 +0800

    VMP以插件形式引用到工程中

--- Plugins/VMProtectPlugin/Source/VMProtectPlugin/VMProtectPlugin.Build.cs
+++ Plugins/VMProtectPlugin/Source/VMProtectPlugin/VMProtectPlugin.Build.cs
@@ -1,23 +1,20 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
-using System.IO;
-using System;
 
-public class StormUtility : ModuleRules
+public class VMProtectPlugin : ModuleRules
 {
-	public StormUtility(TargetInfo Target)
+	public VMProtectPlugin(TargetInfo Target)
 	{
+        string vmDir = ModuleDirectory + "/DLL";
 
-//         string vmDir = Path.GetFullPath(Path.Combine(ModuleDirectory, "..", "..", "..", "VMProtect2"));
-// 
-//         PublicSystemIncludePaths.Add(vmDir);
-//         PublicAdditionalLibraries.Add(vmDir + "/VMProtectSDK64.lib");
-//         PublicLibraryPaths.Add(vmDir + "");
+        PublicSystemIncludePaths.Add(vmDir);
+        PublicAdditionalLibraries.Add(vmDir + "/VMProtectSDK64.lib");
+        PublicLibraryPaths.Add(vmDir + "");
 
         PublicIncludePaths.AddRange(
 			new string[] {
-				"StormUtility/Public"
+				"VMProtectPlugin/Public"
 				// ... add public include paths required here ...
 			}
 			);
@@ -25,7 +22,7 @@ public class StormUtility : ModuleRules
 		
 		PrivateIncludePaths.AddRange(
 			new string[] {
-				"StormUtility/Private",
+				"VMProtectPlugin/Private",
 				// ... add other private include paths required here ...
 			}
 			);
@@ -34,7 +31,7 @@ public class StormUtility : ModuleRules
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"Core", "VMProtectPlugin"
+				"Core",
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
--- Source/test_vmp/test_vmp.Build.cs
+++ Source/test_vmp/test_vmp.Build.cs
@@ -1,20 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
+using System.IO;
+using System;
 
 public class test_vmp : ModuleRules
 {
 	public test_vmp(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "StormUtility" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "StormUtility", "VMProtectPlugin" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-        string vmDir = UEBuildConfiguration.UEThirdPartySourceDirectory + "/VMProtect";
-
-        PublicSystemIncludePaths.Add(vmDir);
-        PublicAdditionalLibraries.Add(vmDir + "/VMProtectSDK64.lib");
-        PublicLibraryPaths.Add(vmDir + "");
+//         string vmDir = Path.GetFullPath(Path.Combine(ModuleDirectory, "..", "..", "Plugins", "VMProtect2"));
+// 
+//         PublicSystemIncludePaths.Add(vmDir);
+//         PublicAdditionalLibraries.Add(vmDir + "/VMProtectSDK64.lib");
+//         PublicLibraryPaths.Add(vmDir + "");
 
 
         // Uncomment if you are using Slate UI
